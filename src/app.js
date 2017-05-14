82a25ddad2bc6016295d3328de967492c7dca7a2
Danish Mughal <mughal.danish@gmail.com>
2017-05-13 20:39:35 -0700

    Firebase setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDPSlonPIsFEYpOaRoCAd2l58fc3hRrALM',
+      authDomain: 'react-auth-b06fa.firebaseapp.com',
+      databaseURL: 'https://react-auth-b06fa.firebaseio.com',
+      projectId: 'react-auth-b06fa',
+      storageBucket: 'react-auth-b06fa.appspot.com',
+      messagingSenderId: '63100178731'
+    });
+  }
+
   render() {
     return (
       <View>
