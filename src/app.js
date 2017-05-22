a8c2448d35d5d2d96ae8341acae55525e044e3fd
Danish Mughal <mughal.danish@gmail.com>
2017-05-21 22:17:57 -0700

    loggedIn state working

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDPSlonPIsFEYpOaRoCAd2l58fc3hRrALM',
@@ -15,13 +17,36 @@ class App extends Component {
       storageBucket: 'react-auth-b06fa.appspot.com',
       messagingSenderId: '63100178731'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+        console.log(this.state.loggedIn);
+      } else {
+        this.setState({ loggedIn: false });
+        console.log('logged out');
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default: 
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        <CardSection>
+          {this.renderContent()}
+        </CardSection>
       </View>
     );
   }
